a86c6a3988acc390db32ef73b7e69690f579ae19
Jannik Arndt <jannik@jannikarndt.de>
2019-11-11 22:24:55 +0100

    Updates

---
+++ precache-manifest.2118b1d6451db0c5907fee95e01f4dfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4392d83790d6e5b4cfa42c3216d0b78",
+    "url": "/trip-estimate-example/index.html"
+  },
+  {
+    "revision": "5fee55e7085f98776343",
+    "url": "/trip-estimate-example/static/css/main.c39b06a5.chunk.css"
+  },
+  {
+    "revision": "77eca16486b1e5ab3aea",
+    "url": "/trip-estimate-example/static/js/2.c478107f.chunk.js"
+  },
+  {
+    "revision": "5fee55e7085f98776343",
+    "url": "/trip-estimate-example/static/js/main.51b0d85a.chunk.js"
+  },
+  {
+    "revision": "8e25fa8e22f895255528",
+    "url": "/trip-estimate-example/static/js/runtime-main.ade9db6b.js"
+  }
+]);
